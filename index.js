a8235b6a24f20eadd3721818060a814dc777a516
Steven Zeiler <zeiler.steven@gmail.com>
2014-09-06 17:59:41 -0700

    [TASK] Remove tons of cruft.

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ var requireAll = require('require-all-to-camel');
 
 exports.config = require(__dirname + '/config/config.js');
 exports.data   = require(__dirname +'/lib/data/');
-exports.api = requireAll(__dirname+'/lib/api/');
 exports.errors = requireAll(__dirname+'/lib/errors/');
-exports.ripple = require(__dirname +'/lib/ripple/');
-exports.validator = require(__dirname+'/lib/validator.js');
 exports.server = require(__dirname+'/lib/app.js');
 
 require(__dirname+'/Gatewaydfile')(exports);
